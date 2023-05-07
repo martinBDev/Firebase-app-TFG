b2e80ea3f639b68b4a37c241dac7cfd131ec5bfa
Martin Beltran <UO276244@uniovi.es>
2023-05-07 20:47:12 +0200

    GeoHash creation

--- functions/index.js
+++ functions/index.js
@@ -1,45 +1,35 @@
 const functions = require("firebase-functions");
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 const admin = require('firebase-admin');
+const { GeoFirestore } = require('geofirestore');
+const {
+  geohashForLocation,
+} = require("geofire-common");
+
 admin.initializeApp();
 
 const db = admin.firestore();
 
 
+//Save metrics in 'metrics' collection
+//This function is called when Android app sends metrics
+//This function saves: bpm, pressure, o2, sugar, timestamp and ownerUID of user
 exports.saveMetrics = functions.region('europe-west3').https.onCall(async (data, context) => {
-  const bpm = data.bpm;
-  const pressure = data.pressure;
-  const o2 = data.o2;
-  const sugar = data.sugar;
-  const ownerUID = data.ownerUID;
-  
+    const bpm = data.bpm;
+    const pressure = data.pressure;
+    const o2 = data.o2;
+    const sugar = data.sugar;
+    const ownerUID = data.ownerUID;
+    const location = data.location;
+    
    
-  
-    console.log('ownerUID:' + ownerUID);
-    if(!ownerUID){
-      throw new functions.https.HttpsError('invalid-argument', 'Invalid data; ownerUID ' + ownerUID);
-    }
-    if(!bpm){
-      throw new functions.https.HttpsError('invalid-argument', 'Invalid data; bpm ' + bpm);
-    }
-    if(!pressure){
-      throw new functions.https.HttpsError('invalid-argument', 'Invalid data; pressure ' + pressure);
-    }
-    if(!o2){
-      throw new functions.https.HttpsError('invalid-argument', 'Invalid data; o2 ' + o2);
-    }
-    if(!sugar){
-      throw new functions.https.HttpsError('invalid-argument', 'Invalid data; sugar ' + sugar);
-    }
-  
+    //Check all values are not null or undefined
+    checkValue('ownerUID', ownerUID);
+    checkValue('bpm', bpm);
+    checkValue('pressure', pressure);
+    checkValue('o2', o2);
+    checkValue('sugar', sugar);
+    checkValue('location', location);
+
     //Create a doc with 6 fields: bpm, pressure, o2, sugar, timestamp and ownerUID of user
 
     try {
@@ -53,6 +43,7 @@ exports.saveMetrics = functions.region('europe-west3').https.onCall(async (data,
           o2: o2,
           sugar: sugar,
           timestamp: timestamp,
+          location: location,
           ownerUID: ownerUID,
         });
       } else {
@@ -62,6 +53,7 @@ exports.saveMetrics = functions.region('europe-west3').https.onCall(async (data,
           o2: o2,
           sugar: sugar,
           timestamp: timestamp,
+          location: location,
           ownerUID: ownerUID,
         });
       }
@@ -83,26 +75,21 @@ exports.createUserDoc = functions.region('europe-west3').https.onCall(async (dat
     const name = data.name;
     const surname = data.surname;
     const email = data.email;
+    const phone = data.phone;
 
-    console.debug('data'+data);
-    console.debug('uid'+data.uid);
-    console.debug('name'+data.name);
-    console.debug('surname'+data.surname);
-    console.debug('email'+data.email);
+    console.debug('data:'+data);
+    console.debug('uid:'+data.uid);
+    console.debug('name:'+data.name);
+    console.debug('surname:'+data.surname);
+    console.debug('email:'+data.email);
+    console.debug('phone:'+data.phone);
 
-
-    if(!uid){
-        throw new functions.https.HttpsError('invalid-argument', 'Invalid data; uid ' + uid);
-    }
-    if(!name){
-        throw new functions.https.HttpsError('invalid-argument', 'Invalid data; name ' + name);
-    }
-    if(!surname){
-        throw new functions.https.HttpsError('invalid-argument', 'Invalid data; surname ' + surname);
-    }
-    if(!email){
-        throw new functions.https.HttpsError('invalid-argument', 'Invalid data; email ' + email);
-    }
+    //check all values are not null or undefined
+    checkValue('uid', uid);
+    checkValue('name', name);
+    checkValue('surname', surname);
+    checkValue('email', email);
+    checkValue('phone', phone);
 
     //try get 'users' collection, if it doesn't exist, it will be created
     //then add a document with the uid as the document id
@@ -114,6 +101,7 @@ exports.createUserDoc = functions.region('europe-west3').https.onCall(async (dat
                 name: name,
                 surname: surname,
                 email: email,
+                phone: phone,
                 createdAt: admin.firestore.Timestamp.now(),
             });
         }
@@ -129,5 +117,91 @@ exports.createUserDoc = functions.region('europe-west3').https.onCall(async (dat
     
 });
 
+
+
+exports.findNearestLocation = functions.region('europe-west3').https.onCall(async (data, context) => {
+  const currentLatitude = data.latitude;
+  const currentLongitude = data.longitude;
+  
+  //Check all values are not null or undefined
+  checkValue('latitude', currentLatitude);
+  checkValue('longitude', currentLongitude);
+
+  currentGeoPoint = new admin.firestore.GeoPoint(currentLatitude, currentLongitude);
+
+  const geocollection = geofirestore.collection('centers');
+
+  //Read docs from 'centers' collection
+  try {
+
+    const snapshot = await geocollection.get();
+    const locations = snapshot.docs.map(doc => ({
+      id: doc.id,
+      data: doc.data(),
+    }));
+
+    let nearestLocation = null;
+    let nearestDistance = Number.MAX_VALUE;
+ 
+    locations.forEach(location => {
+      const distance = GeoFirestoreUtils.distanceBetween(
+        [location.data.coordinates.latitude, location.data.coordinates.longitude],
+        [center.latitude, center.longitude]
+      );
+  
+      if (distance < nearestDistance) {
+        nearestDistance = distance;
+        nearestLocation = location;
+      }
+    });
+
+    //Return the nearest center
+    return {
+      center: nearestLocation,
+    };
+  } catch (error) {
+    console.error(error);
+    throw new functions.https.HttpsError('internal', 'Error searching nearest center');
+  }
+ 
+});
+
+//Function to check a value is not null or undefined,
+//if it is throw an error
+function checkValue(name,value) {
+  if (!value) {
+    throw new functions.https.HttpsError('invalid-argument', 'Invalid data; ' + name + ":" + value);
+  }
+}
+
+exports.createGeoHash = functions.firestore
+  .document('centers/{centerId}')
+  .onCreate((snap, context) => {
+    
+    //Add field to documento
+    const location = snap.data().location;
+    const hash = geohashForLocation([location.latitude, location.longitude]);
+    
+      
+      return snap.ref.set({
+        geohash: hash
+      }, {merge: true});
+  });
+
+  exports.updateGeoHash = functions.firestore
+  .document('centers/{centerId}')
+  .onUpdate((change, context) => {
+    
+    const location = change.after.data().location;
+    
+    const hash = geohashForLocation([location.latitude, location.longitude]);
+
+
+    //Add field to document
+    return change.after.ref.set({
+      geohash: hash
+    }, {merge: true});
+
+  });
 
   
